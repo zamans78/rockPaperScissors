f481161b02a94d0d84a86e73057c1ced3cc3c97c
Mohammed Saduzzaman Sadi <64192420+zamans78@users.noreply.github.com>
2020-11-22 12:43:04 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mohammed Saduzzaman Sadi</title>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
+
